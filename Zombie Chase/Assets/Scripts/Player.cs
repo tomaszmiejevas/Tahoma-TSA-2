b674446022fdee15cc9871c21f342de82e99c45e
Tomas Zmiejevas <tomas.zmiejevas@gmail.com>
2022-01-19 21:56:56 -0800

    Zombie chase

---
+++ Zombie Chase/Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+{
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerJump();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+
+    }
+
+    void AnimatePlayer()
+    {
+        if (movementX > 0)
+        {
+
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+
+        } else if (movementX < 0){
+
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetKeyDown("Jump"))
+        {
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+}
